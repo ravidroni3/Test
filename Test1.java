b6811d60697ae05b864f499fe5a0720b09f9276f
ravidroni3 <113836933+ravidroni3@users.noreply.github.com>
2023-07-30 13:51:04 +0530

    Revert "Updated1"
    This reverts commit 66763335b7d30f085b9917c6dbf310170da429ad.

--- Test1.java
+++ Test1.java
@@ -2,6 +2,5 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Java 8");
         System.out.println("Java 11");
-        System.out.println("Java 17");
     }
 }
--- Test2.java
+++ Test2.java
@@ -2,7 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Python1");
         System.out.println("Python3");
-        System.out.println("Python5");
 
     }
 }
