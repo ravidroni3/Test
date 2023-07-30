91fd7ee2c4adb9d2273db26a5496d1ff8659cba9
ravidroni3 <113836933+ravidroni3@users.noreply.github.com>
2023-07-30 14:12:58 +0530

    Revert "Feature (#5)"
    This reverts commit 2c902a7aaf9c88cc1ba8a8fb6e7a6d2dfdde2199.

--- Test1.java
+++ Test1.java
@@ -3,7 +3,5 @@ public class Test1 {
         System.out.println("Java 8");
         System.out.println("Java 11");
         System.out.println("Java 17");
-        System.out.println("Java 20");
-        System.out.println("Java Latest Version 2023");
     }
 }
--- Test2.java
+++ Test2.java
@@ -2,9 +2,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Python1");
         System.out.println("Python3");
-        System.out.println("Python3.10");
-        System.out.println("Python3.12");
-        System.out.println("Python latest version 2023");
+        System.out.println("Python5");
 
     }
 }
