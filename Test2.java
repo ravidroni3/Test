2c902a7aaf9c88cc1ba8a8fb6e7a6d2dfdde2199
ravidroni3 <113836933+ravidroni3@users.noreply.github.com>
2023-07-30 14:11:12 +0530

    Feature (#5)
    * updated2
    
    * updated3
    
    * updated4

--- Test2.java
+++ Test2.java
@@ -2,7 +2,9 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Python1");
         System.out.println("Python3");
-        System.out.println("Python5");
+        System.out.println("Python3.10");
+        System.out.println("Python3.12");
+        System.out.println("Python latest version 2023");
 
     }
 }
